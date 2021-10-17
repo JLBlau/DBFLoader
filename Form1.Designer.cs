9f15d361cee06523c7f645556ba18c3298c16b85
JLBlau <jblau@gmx.com>
2021-10-17 09:43:48 -0400

    cleanup to share

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,28 +46,28 @@
             // DBFLabel
             // 
             this.DBFLabel.AutoSize = true;
-            this.DBFLabel.Location = new System.Drawing.Point(22, 44);
-            this.DBFLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.DBFLabel.Location = new System.Drawing.Point(7, 15);
+            this.DBFLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.DBFLabel.Name = "DBFLabel";
-            this.DBFLabel.Size = new System.Drawing.Size(212, 37);
+            this.DBFLabel.Size = new System.Drawing.Size(72, 13);
             this.DBFLabel.TabIndex = 0;
             this.DBFLabel.Text = "DBF Location";
             // 
             // dbfPath
             // 
-            this.dbfPath.Location = new System.Drawing.Point(375, 44);
-            this.dbfPath.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.dbfPath.Location = new System.Drawing.Point(118, 15);
+            this.dbfPath.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.dbfPath.Name = "dbfPath";
-            this.dbfPath.Size = new System.Drawing.Size(352, 44);
+            this.dbfPath.Size = new System.Drawing.Size(114, 20);
             this.dbfPath.TabIndex = 1;
             this.dbfPath.Text = "C:\\Temp\\DBFs";
             // 
             // selectFolder
             // 
-            this.selectFolder.Location = new System.Drawing.Point(823, 44);
-            this.selectFolder.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.selectFolder.Location = new System.Drawing.Point(260, 10);
+            this.selectFolder.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.selectFolder.Name = "selectFolder";
-            this.selectFolder.Size = new System.Drawing.Size(190, 46);
+            this.selectFolder.Size = new System.Drawing.Size(82, 27);
             this.selectFolder.TabIndex = 2;
             this.selectFolder.Text = "Select Folder";
             this.selectFolder.UseVisualStyleBackColor = true;
@@ -76,57 +76,57 @@
             // sqlServerLabel
             // 
             this.sqlServerLabel.AutoSize = true;
-            this.sqlServerLabel.Location = new System.Drawing.Point(22, 135);
-            this.sqlServerLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.sqlServerLabel.Location = new System.Drawing.Point(7, 47);
+            this.sqlServerLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.sqlServerLabel.Name = "sqlServerLabel";
-            this.sqlServerLabel.Size = new System.Drawing.Size(182, 37);
+            this.sqlServerLabel.Size = new System.Drawing.Size(62, 13);
             this.sqlServerLabel.TabIndex = 3;
             this.sqlServerLabel.Text = "SQL Server";
             // 
             // sqlServerName
             // 
-            this.sqlServerName.Location = new System.Drawing.Point(375, 130);
-            this.sqlServerName.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.sqlServerName.Location = new System.Drawing.Point(118, 46);
+            this.sqlServerName.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.sqlServerName.Name = "sqlServerName";
-            this.sqlServerName.Size = new System.Drawing.Size(352, 44);
+            this.sqlServerName.Size = new System.Drawing.Size(114, 20);
             this.sqlServerName.TabIndex = 4;
             this.sqlServerName.Text = "SQLServer";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 207);
-            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Location = new System.Drawing.Point(7, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 37);
+            this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "SQL User";
             // 
             // sqlUserName
             // 
-            this.sqlUserName.Location = new System.Drawing.Point(375, 207);
-            this.sqlUserName.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.sqlUserName.Location = new System.Drawing.Point(118, 73);
+            this.sqlUserName.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.sqlUserName.Name = "sqlUserName";
-            this.sqlUserName.Size = new System.Drawing.Size(352, 44);
+            this.sqlUserName.Size = new System.Drawing.Size(114, 20);
             this.sqlUserName.TabIndex = 6;
             this.sqlUserName.Text = "SA";
             // 
             // sqlPasswordLabel
             // 
             this.sqlPasswordLabel.AutoSize = true;
-            this.sqlPasswordLabel.Location = new System.Drawing.Point(22, 286);
-            this.sqlPasswordLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.sqlPasswordLabel.Location = new System.Drawing.Point(7, 100);
+            this.sqlPasswordLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.sqlPasswordLabel.Name = "sqlPasswordLabel";
-            this.sqlPasswordLabel.Size = new System.Drawing.Size(231, 37);
+            this.sqlPasswordLabel.Size = new System.Drawing.Size(77, 13);
             this.sqlPasswordLabel.TabIndex = 7;
             this.sqlPasswordLabel.Text = "SQL Password";
             // 
             // sqlPassword
             // 
-            this.sqlPassword.Location = new System.Drawing.Point(375, 281);
-            this.sqlPassword.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.sqlPassword.Location = new System.Drawing.Point(118, 99);
+            this.sqlPassword.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.sqlPassword.Name = "sqlPassword";
-            this.sqlPassword.Size = new System.Drawing.Size(352, 44);
+            this.sqlPassword.Size = new System.Drawing.Size(114, 20);
             this.sqlPassword.TabIndex = 8;
             this.sqlPassword.Text = "password";
             this.sqlPassword.UseSystemPasswordChar = true;
@@ -134,28 +134,28 @@
             // sqlDBLabel
             // 
             this.sqlDBLabel.AutoSize = true;
-            this.sqlDBLabel.Location = new System.Drawing.Point(19, 361);
-            this.sqlDBLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.sqlDBLabel.Location = new System.Drawing.Point(6, 127);
+            this.sqlDBLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.sqlDBLabel.Name = "sqlDBLabel";
-            this.sqlDBLabel.Size = new System.Drawing.Size(302, 37);
+            this.sqlDBLabel.Size = new System.Drawing.Size(102, 13);
             this.sqlDBLabel.TabIndex = 9;
             this.sqlDBLabel.Text = "New SQL DB Name";
             // 
             // newSQLDBName
             // 
-            this.newSQLDBName.Location = new System.Drawing.Point(375, 361);
-            this.newSQLDBName.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.newSQLDBName.Location = new System.Drawing.Point(118, 127);
+            this.newSQLDBName.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.newSQLDBName.Name = "newSQLDBName";
-            this.newSQLDBName.Size = new System.Drawing.Size(352, 44);
+            this.newSQLDBName.Size = new System.Drawing.Size(114, 20);
             this.newSQLDBName.TabIndex = 10;
             this.newSQLDBName.Text = "Staged_DBFs";
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(823, 336);
-            this.startButton.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.startButton.Location = new System.Drawing.Point(260, 118);
+            this.startButton.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(261, 71);
+            this.startButton.Size = new System.Drawing.Size(82, 25);
             this.startButton.TabIndex = 11;
             this.startButton.Text = "Start";
             this.startButton.UseVisualStyleBackColor = true;
@@ -164,18 +164,17 @@
             // lbMessages
             // 
             this.lbMessages.FormattingEnabled = true;
-            this.lbMessages.ItemHeight = 37;
-            this.lbMessages.Location = new System.Drawing.Point(30, 503);
-            this.lbMessages.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.lbMessages.Location = new System.Drawing.Point(9, 177);
+            this.lbMessages.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.lbMessages.Name = "lbMessages";
-            this.lbMessages.Size = new System.Drawing.Size(1066, 337);
+            this.lbMessages.Size = new System.Drawing.Size(339, 121);
             this.lbMessages.TabIndex = 12;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1184, 931);
+            this.ClientSize = new System.Drawing.Size(368, 320);
             this.Controls.Add(this.lbMessages);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.newSQLDBName);
@@ -189,7 +188,7 @@
             this.Controls.Add(this.selectFolder);
             this.Controls.Add(this.dbfPath);
             this.Controls.Add(this.DBFLabel);
-            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.Name = "Form1";
             this.Text = "MainForm";
             this.ResumeLayout(false);
